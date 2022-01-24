23fe0ceea6fd94fdaf3bdc297773d08e04ec5f0e
sienning <sienning@hotmail.fr>
2022-01-24 15:51:01 +0100

    correction bugs connexion avec back

--- src/components/isConnected/Player/PlayerCard.js
+++ src/components/isConnected/Player/PlayerCard.js
@@ -9,7 +9,7 @@ const PlayerCard = ({ open, player }) => {
                 {
                     player.discordId ?
                         <Image className="pp_user" floated="right" size="tiny" circular src={`https://cdn.discordapp.com/avatars/${player.discordId}/${player.avatar}`} />
-                        : <Image className="pp_user" floated="right" size="tiny" circular src={`${process.env.REACT_APP_SERVER}/images/${player.avatar}`} />
+                        : <Image className="pp_user" floated="right" size="tiny" circular src={`${process.env.REACT_APP_SERVER}images/${player.avatar}`} />
                 }
                 <Card.Header>{player.username}</Card.Header>
                 <p>Info</p>
